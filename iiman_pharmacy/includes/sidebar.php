37b0a7634ac8f851856b30b43533cf3059c8e5dd
Liban Hussein <libanmohamedreal@gmail.com>
2025-01-25 23:28:27 +0300

    Today i made the header and the sidebar

---
+++ iiman_pharmacy/includes/sidebar.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="../output.css">
+<script src="https://kit.fontawesome.com/4be9701c6b.js" crossorigin="anonymous"></script>
+<?php include "header.php";
+?>
+<div class="side w-1/6 h-full bg-slate-400 text-white">
+    <div class="elements  flex flex-col gap-6 pl-7 pt-8 pr-2">
+        <div class="dashboard h-8 bg-blue-400 pl-3 rounded flex items-center">
+            <a class="flex gap-2 items-center" href="../index.php"><i class="fa-solid fa-house"></i>
+                <span>Dashboard</span></a>
+        </div>
+        <div class="dashboard h-8 bg-blue-400 pl-3 rounded flex items-center">
+            <a class="flex gap-2 items-center" href="../index.php"><i class="fa-solid fa-house"></i>
+                <span>Dashboard</span></a>
+        </div>
+        <div class="dashboard h-8 bg-blue-400 pl-3 rounded flex items-center">
+            <a class="flex gap-2 items-center" href="../index.php"><i class="fa-solid fa-house"></i>
+                <span>Dashboard</span></a>
+        </div>
+        <div class="dashboard h-8 bg-blue-400 pl-3 rounded flex items-center">
+            <a class="flex gap-2 items-center" href="../index.php"><i class="fa-solid fa-house"></i>
+                <span>Dashboard</span></a>
+        </div>
+        <div class="dashboard h-8 bg-blue-400 pl-3 rounded flex items-center">
+            <a class="flex gap-2 items-center" href="../index.php"><i class="fa-solid fa-house"></i>
+                <span>Dashboard</span></a>
+        </div>
+        <div class="dashboard h-8 bg-blue-400 pl-3 rounded flex items-center">
+            <a class="flex gap-2 items-center" href="../index.php"><i class="fa-solid fa-house"></i>
+                <span>Dashboard</span></a>
+        </div>
+        <div class="dashboard h-8 bg-blue-400 pl-3 rounded flex items-center">
+            <a class="flex gap-2 items-center" href="../index.php"><i class="fa-solid fa-house"></i>
+                <span>Dashboard</span></a>
+        </div>
+        <div class="dashboard h-8 bg-blue-400 pl-3 rounded flex items-center">
+            <a class="flex gap-2 items-center" href="../index.php"><i class="fa-solid fa-house"></i>
+                <span>Dashboard</span></a>
+        </div>
+        
+    </div>
+</div>
