4a1f1466f7393127040e02b26a9eeed3f4949c76
LibanMoo <libanmohamedreal@gmail.com>
2025-02-23 14:56:21 +0300

    Made login page responsive to any device

--- login.php
+++ login.php
@@ -9,9 +9,9 @@
 <body>
     <div class="containerr w-full h-screen flex flex-col justify-center items-center bg-[var(--color-background)]">
         
-        <form method="post" class= "w-[50%] h-[70%] flex flex-col gap-[4%] pl-10 pr-10 pt-5 bg-[var(--color-white)] items-center">
+        <form method="post" class= "w-[50%] h-[70%] flex flex-col gap-[5%] pl-10 pr-10 pt-5 bg-[var(--color-white)] items-center max-md:w-[70%] max-sm:w-[90%]">
         <h2 class="font-semibold">Login</h2>
-            <input class="w-full bg-[var(--color-background)] h-[12%] pl-[2%] rounded" type="text" name="username" placeholder="Username">
+            <input class="w-full bg-[var(--color-background)] h-[12%] pl-[2%] rounded focus:border-2 focus:border-[var(--color-success)]" type="text" name="username" placeholder="Username">
             <input class="w-full bg-[var(--color-background)] h-[12%] pl-[2%] rounded" type="password" name="password" placeholder="Password">
             <input class="w-[30%] bg-[var(--color-success)] text-white font-semibold h-[15%] rounded cursor-pointer" type="submit" value="Send" name="send">
         </form>
