1b313d005a9884eb4d07c0f9ab3302c625739c05
LibanMoo <libanmohamedreal@gmail.com>
2025-03-10 16:46:50 +0300

    Made the Modal background Dark transparent

--- modals/adminModal.php
+++ modals/adminModal.php
@@ -10,12 +10,12 @@
 <link rel="stylesheet" href="../output.css">
 
 <!-- This is an example component -->
-<div class="w-full h-screen flex justify-center items-center bg-black bg-opacity-50>
+<div class="w-full h-screen flex justify-center items-center bg-black bg-opacity-50">
  
     <!-- Modal toggle -->
     <!-- <button class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="authentication-modal">
     Toggle login modal
-    </button> -->
+    </button> 
 
     <!-- Main modal -->
             <div class="bg-white rounded-lg shadow relative dark:bg-gray-700 w-[50%]">
