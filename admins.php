08fc648547b56a49502580b3fb1ac0558f05ea67
LibanMoo <libanmohamedreal@gmail.com>
2025-03-15 23:06:37 +0300

    Removed the variable displaying admin password

--- admins.php
+++ admins.php
@@ -37,8 +37,7 @@ if (isset($_POST['send'])) {
         echo " <script> Swal.fire({
   position: 'top',
   icon: 'success',
-  title: 'Your work has been saved
- admin Password is:<?php echo $generatedPass;?>',
+  title: 'Your work has been saved',
   showConfirmButton: true,
   timer: 30000
 });
