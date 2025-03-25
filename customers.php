56fcce7c8dda87c7b32eb57cd4abc5cc144610e7
LibanMoo <libanmohamedreal@gmail.com>
2025-03-25 17:27:21 +0300

    added the header and sidebar in the customers page and called the init file

---
+++ customers.php
@@ -0,0 +1,20 @@
+<?php include './includes/init.php';
+      include './includes/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="output.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="containerr">
+        <div class="sidebar">
+            <?php include './includes/sidebar.php'; ?>
+        </div>
+    </div>
+</body>
+</html>
