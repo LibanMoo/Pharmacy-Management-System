716ba2272802021a91264e6e7821de9724650c77
LibanMoo <libanmohamedreal@gmail.com>
2025-03-31 20:57:02 +0300

    made the customers table

--- customers.php
+++ customers.php
@@ -19,6 +19,38 @@
         <div class="admin w-[13%] h-[7%] bg-[var(--color-primary)] flex items-center justify-center rounded hover:cursor-pointer max-sm:w-[35%] max-md:w-[25%] ">
                 <button onclick="callModal()" class="hover:cursor-pointer text-[var(--color-white)] text-bold">Add Customer</button>
             </div>
+            <table id="myTable" class="stripe row-border hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Number</th>
+                        <th>Address </th>
+                        <th>Total</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach (read('customers') as $customer) {
+                    ?>
+                        <tr>
+                            <td><?= $customer['customer_name']; ?></td>
+                            <td><?= $customer['customer_number'] ?></td>
+                            <td><?= $admin['address'] ?></td>
+                            <td><?= $admin['address'] ?></td>
+                            <td><?= $admin['date'] ?></td>
+                            <td><?= $admin['time'] ?></td>
+                            <td class="flex h-full gap-5 text-3xl">
+                                <div class="edit text-[var(--color-primary)] cursor-pointer"><i onclick="callModal();fillForm(<?= $admin['admin_id']; ?>);" class="fa-solid fa-pen-to-square"></i></div>
+                                <div class="delete text-[var(--color-danger)] cursor-pointer">
+                                    <i onclick="deleteModal();fillDeleteForm(<?= $admin['admin_id']?>, `<?= $admin['username'] ?>`);" class="fa-solid fa-trash"></i>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php }; ?>
+                </tbody>
+            </table>
+        </div>
         </div>
     </div>
 </body>
