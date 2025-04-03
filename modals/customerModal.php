cfb4196ba32ea1648e7d9c4d2a9dbb6c3f6716fd
LibanMoo <libanmohamedreal@gmail.com>
2025-04-03 17:55:01 +0300

    added a new input and datalist that will select existing customer for damiin

--- modals/customerModal.php
+++ modals/customerModal.php
@@ -19,15 +19,15 @@
                 <form class="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" method="post">
                     <h3 id="addAdmin" class="text-xl font-medium text-gray-900 dark:text-white">Register a new Admin</h3>
                     <div>
-                        <label for="username" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Customer Name</label>
+                        <label for="name" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Customer Name</label>
                         <input type="text" name="customerName" id="customerName" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Liban" required="">
                     </div>
                     <div>
-                        <label for="name" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Address</label>
+                        <label for="address" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Address</label>
                         <input type="text" name="customerAddress" id="customerAddress" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Madiina" required="">
                     </div>
                     <div>
-                        <label for="name" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Customer Number</label>
+                        <label for="number" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Customer Number</label>
                         <input type="number" name="customerNumber" id="customerNumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-inner-spin-button]:appearance-none [&::-moz-outer-spin-button]:appearance-none" placeholder="+252612569167" required="">
                     </div>
                     <div>
@@ -38,6 +38,13 @@
                         <option value="0">Maya</option>
                       </select>
                       </div>
+                      <div>
+                        <label for="datalist" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Customer Number</label>
+                        <input type="number" name="customerNumber" id="customerNumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-inner-spin-button]:appearance-none [&::-moz-outer-spin-button]:appearance-none" placeholder="+252612569167" required="">
+                        <datalist id="customerNames">
+                    
+                        </datalist>
+                    </div>
                     <div class="flex justify-between">
                         <div class="flex items-start">
                     <button id="register" name="send" type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register Admin</button>
