7e487cf80d8a67dbc8761f3c49c8b3ebd70c47c4
LibanMoo <libanmohamedreal@gmail.com>
2025-04-04 20:37:07 +0300

    made the validate php handle it and fetch data from the database send response back to customerModal.php as json

--- includes/validate.php
+++ includes/validate.php
@@ -1,7 +1,16 @@
 <?php
     header('content-type: application/json');
-    if (read_where('customers', 'customer_name = ')){
-
+    if (isset($_POST['damiinInput'])){
+      $customer = $_POST['damiinInput'];
+    $result = read_where('customers', "customer_name = $customer");
+    
+    if ($result){
+        json_encode($result);
     }
+    else {
+        echo 'No data found';
+    }
+    
+}
 
 ?>
