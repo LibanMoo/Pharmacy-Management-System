83ac2b4ed5fad694c020510e89a729df8b9ca17b
LibanMoo <libanmohamedreal@gmail.com>
2025-04-06 19:08:37 +0300

    changed the label to damiin

--- modals/customerModal.php
+++ modals/customerModal.php
@@ -34,7 +34,7 @@
                 <input type="number" name="customerNumber" id="customerNumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-inner-spin-button]:appearance-none [&::-moz-outer-spin-button]:appearance-none" placeholder="+252612569167" required="">
             </div>
             <div>
-                <label for="role" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Role</label>
+                <label for="role" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Damiin</label>
                 <select name="role" id="role" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
                     <option selected disabled>--- Damiin ---</option>
                     <option value="1">Haa</option>
