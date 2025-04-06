0d589c2e0f5b65b15027f489471426972e44909f
LibanMoo <libanmohamedreal@gmail.com>
2025-04-06 23:26:04 +0300

    updated the titles in customerModal.php

--- modals/customerModal.php
+++ modals/customerModal.php
@@ -20,7 +20,7 @@
             </button>
         </div>
         <form class="space-y-4 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" method="post">
-            <h3 id="addAdmin" class="text-xl font-medium text-gray-900 dark:text-white">Register a new Admin</h3>
+            <h3 id="addAdmin" class="text-xl font-medium text-gray-900 dark:text-white">Register a new Customer</h3>
             <div>
                 <label for="name" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Customer Name</label>
                 <input type="text" name="customerName" id="customerName" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Liban" required="">
@@ -51,7 +51,7 @@
             </div>
             <div class="flex justify-between">
                 <div class="flex items-start">
-                    <button id="register" name="send" type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register Admin</button>
+                    <button id="register" name="send" type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register Customer</button>
                 </div>
             </div>
         </form>
