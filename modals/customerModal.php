87ca72dafa16748bfe15301017f933d04656e20a
LibanMoo <libanmohamedreal@gmail.com>
2025-04-20 15:08:26 +0300

    removed the damiin name input cos we dont need it anymore

--- modals/customerModal.php
+++ modals/customerModal.php
@@ -49,10 +49,6 @@
                     <option id="validationOption" value="hee">haa</option> -->
                  </datalist>
             </div>
-            <div id="damiinNameContent" class="hidden">
-                <label for="number" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Damiin Name</label>
-                <input type="text" name="damiinName" id="damiinName" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-inner-spin-button]:appearance-none [&::-moz-outer-spin-button]:appearance-none" placeholder="Ahmed Ali" required="">
-            </div>
             <div class="flex justify-between">
                 <div class="flex items-start">
                     <button id="register" name="send" type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register Customer</button>
