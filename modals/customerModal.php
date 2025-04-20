4b15bd6f655aa95c8ca18b49a4c2b72a9abacbf5
LibanMoo <libanmohamedreal@gmail.com>
2025-04-20 15:14:54 +0300

    made the padding of damiinOptions input 2 and its the actual padding of the form inputs

--- modals/customerModal.php
+++ modals/customerModal.php
@@ -35,7 +35,7 @@
             </div>
             <div>
                 <label for="role" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Damiin</label>
-                <select name="damiinOptions" id="damiinOptions" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                <select name="damiinOptions" id="damiinOptions" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
                     <option selected disabled>--- Damiin ---</option>
                     <option id="optionHaa" value="1">Haa</option>
                     <option id="OptionMaya" value="0">Maya</option>
