36e6357f39b67ed2ec25b33d8681a5d0458eb485
LibanMoo <libanmohamedreal@gmail.com>
2025-04-22 16:22:20 +0300

    added a js function that will the behaviors of my table

--- deemaha.php
+++ deemaha.php
@@ -5,6 +5,7 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="output.css">
+    <link rel="stylesheet" href="./lib/data/dataTables.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -14,7 +15,7 @@
         <div class="sidebar">
             <?php include "./includes/sidebar.php"; ?>
         </div>
-        <div class="content w-full h-full ml-[17%] max-md:ml-0">
+        <div class="content w-full ml-[17%] flex flex-col  max-md:ml-0">
             <div class="deemaha w-full flex justify-between h-[8%] pr-4">
         <div class="Deen_ku_dar w-[13%]  bg-[var(--color-primary)] flex items-center justify-center rounded hover:cursor-pointer max-sm:w-[35%] max-md:w-[25%] ">
                 <button onclick="callModal()" class="hover:cursor-pointer text-[var(--color-white)] text-bold">Deen Ku dar</button>
@@ -70,5 +71,19 @@
     </div>
     <script src="lib/jquery/jquery.min.js"></script>
     <script src="lib/data/dataTables.js"></script>
+
+    <script>
+          $(document).ready(function() {
+            $('#myTable').DataTable({
+                pagingType: 'full',
+                language: {
+                    searchPlaceholder: "...search"
+                }
+
+
+            });
+
+        });
+    </script>
 </body>
 </html>
