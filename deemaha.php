fc74a0b69e4b8b1bd8ef32d022f54e10476caf46
LibanMoo <libanmohamedreal@gmail.com>
2025-04-22 16:07:56 +0300

    created table to display customer records of barrow and payment

--- deemaha.php
+++ deemaha.php
@@ -23,6 +23,49 @@
                 <button onclick="callModal()" class="hover:cursor-pointer text-[var(--color-white)] text-bold">Deen Ka jar</button>
             </div>
             </div>
+            <div class="tablle">
+            <table id="myTable" class="stripe row-border hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Number</th>
+                        <th>Address </th>
+                        <th>Total</th>
+                        <th>Admin Username</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach (read('customers') as $customer) {
+                        foreach (getSum('deemaha', 'amount', 'total', $customer['customer_id']) as $deen) {
+                            // echo $deen['total'];
+                            if (empty($deen['total'])) {
+                                $deen['total'] = 0;
+                                // echo $deen['total'];
+                            }
+                        }
+                    ?>
+                        <tr>
+                            <td><?= $customer['customer_name']; ?></td>
+                            <td><?= $customer['customer_number'] ?></td>
+                            <td><?= $customer['address'] ?></td>
+                            <td>$<?= $deen['total'] ?></td>
+                            <td><?= $customer['admin_username'] ?></td>
+                            <td><?= $customer['date'] ?></td>
+                            <td><?= $customer['time'] ?></td>
+                            <td class="flex h-full gap-5 text-3xl">
+                                <div class="view text-[var(--color-primary)] cursor-pointer"><i onclick="callModal();fillForm(<?= $admin['admin_id']; ?>);" class="fa-solid fa-eye"></i></div>
+                                <div class="delete text-[var(--color-danger)] cursor-pointer">
+                                    <i onclick="deleteModal();fillDeleteForm(<?= $customer['customer_id'] ?>, `<?= $customer['customer_name'] ?>`);" class="fa-solid fa-trash"> </i>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php }; ?>
+                </tbody>
+            </table>
+            </div>
         </div>
     </div>
 </body>
