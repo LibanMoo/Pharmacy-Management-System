c6958ac08c2b7e6da80eb8fbaabcab9d2fc39cb1
LibanMoo <libanmohamedreal@gmail.com>
2025-04-22 15:25:53 +0300

    made the buttons that will register new records of barrow and payment

--- deemaha.php
+++ deemaha.php
@@ -10,11 +10,20 @@
     <title>Document</title>
 </head>
 <body class="bg-[var(--color-background)]">
-    <div class="container flex">
+    <div class="container flex w-full h-screen">
         <div class="sidebar">
             <?php include "./includes/sidebar.php"; ?>
         </div>
-        <div class="content"></div>
+        <div class="content w-full h-full ml-[17%] max-md:ml-0">
+            <div class="deemaha w-full flex justify-between h-[8%] pr-4">
+        <div class="Deen_ku_dar w-[13%]  bg-[var(--color-primary)] flex items-center justify-center rounded hover:cursor-pointer max-sm:w-[35%] max-md:w-[25%] ">
+                <button onclick="callModal()" class="hover:cursor-pointer text-[var(--color-white)] text-bold">Deen Ku dar</button>
+            </div>
+        <div class="Deen_ku_dar w-[13%] bg-[var(--color-primary)] flex items-center justify-center rounded hover:cursor-pointer max-sm:w-[35%] max-md:w-[25%] ">
+                <button onclick="callModal()" class="hover:cursor-pointer text-[var(--color-white)] text-bold">Deen Ka jar</button>
+            </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
