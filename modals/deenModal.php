fba5faf5c68e90019e0c173e1cf47fcd88cc23a4
LibanMoo <libanmohamedreal@gmail.com>
2025-04-26 15:29:21 +0300

    made a new input for tha product name

--- modals/deenModal.php
+++ modals/deenModal.php
@@ -37,6 +37,10 @@
                         <option value="0">Maya</option>
                       </select>
                       </div>
+                      <div>
+                        <label for="productName" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Product Name</label>
+                        <input type="text" name="productName" id="productName" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="product name" required="">
+                    </div>
                     <div class="flex justify-between">
                         <div class="flex items-start">
                     <button id="register" name="send" type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
