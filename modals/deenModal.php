c297c8456f163d190918c1c67298a5c4abfef354
LibanMoo <libanmohamedreal@gmail.com>
2025-04-26 16:39:27 +0300

    linked jquery and made that will that will remove hidden when haa option is selected

--- modals/deenModal.php
+++ modals/deenModal.php
@@ -30,14 +30,14 @@
                         <input type="hidden" name="transaction" id="transaction" value="insert" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Name" required="">
                     </div>
                     <div>
-                        <label for="product" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Product</label>
-                      <select name="product" id="product" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                        <label for="productOption" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Product</label>
+                      <select name="productOption" id="productOption" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
                         <option selected disabled>--- Select Role ---</option>
-                        <option value="1">Haa</option>
-                        <option value="0">Maya</option>
+                        <option id="optionHaa" value="1">Haa</option>
+                        <option id="optionMaya" value="0">Maya</option>
                       </select>
                       </div>
-                      <div>
+                      <div id="productRow" class="hidden">
                         <label for="productName" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Product Name</label>
                         <input type="text" name="productName" id="productName" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="product name" required="">
                     </div>
@@ -47,5 +47,27 @@
                 </form>
             </div>
         </div>
+        <script src="../lib/jquery/jquery.min.js"></script>
+
+        <script>
+            const productOptions = $('#productOption'),
+                  optionHaa = $('#optionHaa'),
+                  optionMaya = $('#optionMaya'),
+                  productRow = $('#productRow');
+
+            productOptions.on('change', (e)=>{
+        console.log('here')
+        const selectedValue = e.target.value;
+        console.log(selectedValue);
+        if (selectedValue == '1'){
+            console.log('reached here')
+            productRow.removeClass('hidden');
+        }
+        else if (selectedValue == '0'){
+            console.log('reached the else')
+           productRow.addClass('hidden')
+        }
+    })
+        </script>
 
 <!-- <script src="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/flowbite.bundle.js"></script> -->
