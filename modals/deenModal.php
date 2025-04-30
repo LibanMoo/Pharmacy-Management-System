6d263227caf98902b213ec3c971dcd810da14dc6
LibanMoo <libanmohamedreal@gmail.com>
2025-04-30 11:58:05 +0300

    updated the title of deenModal.php

--- modals/deenModal.php
+++ modals/deenModal.php
@@ -17,7 +17,7 @@
                     </button>
                 </div>
                 <form class="space-y-4 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" method="post">
-                    <h3 id="addAdmin" class="text-xl font-medium text-gray-900 dark:text-white">Register a new Admin</h3>
+                    <h3 id="addAdmin" class="text-xl font-medium text-gray-900 dark:text-white">Register a new Transaction</h3>
                     <div id="total" class="hidden p-2 absolute top-9 right-2">
                     <input type="text" value="0" name="total" id="total"  class="text-gray-400 w-5 bg-transparent  hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5  items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="authentication-modal">
 
