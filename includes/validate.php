a45ff8a179ee24c39fb3cdc44c4f08c0f380ddbe
LibanMoo <libanmohamedreal@gmail.com>
2025-05-01 13:47:46 +0300

    updated the global variable and made them block variables

--- includes/validate.php
+++ includes/validate.php
@@ -1,10 +1,11 @@
 <?php include '../includes/functions.php';
    header('Content-Type: application/json');
 
-   $product = $_POST['product'];
-   $customer = $_POST['customer'];
+
+ 
    
-   if (isset($customer)) {
+   if (isset($_POST['customer'])) {
+    $customer = $_POST['customer'];
    
        // Make sure the value is safely quoted
        $result = read_where_limit('customers', "customer_name OR customer_number LIKE '%$customer%'", '3');
@@ -20,7 +21,8 @@
                "message" => "Customer not found"
            ]);
        }
-   } elseif(isset($product)){
+   } elseif(isset($_POST['product'])){
+    $product = $_POST['product'];
 
     $result = read_where_limit('products', "product_name LIKE '%$product%'", '3');
 
