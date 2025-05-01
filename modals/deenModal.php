ca3515065083331783b3a0eccacd1b3e45a5420f
LibanMoo <libanmohamedreal@gmail.com>
2025-05-01 13:49:02 +0300

    changed the placeholder to quantity

--- modals/deenModal.php
+++ modals/deenModal.php
@@ -50,7 +50,7 @@
                     </div>
                     <div id="quantityRow" class="hidden">
                         <label for="quantity" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Quantity</label>
-                        <input type="text" name="quantity" id="quantity" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="description" required="">
+                        <input type="text" name="quantity" id="quantity" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="quantity" required="">
                     </div>
                     <div class="flex justify-between">
                         <div class="flex items-start">
